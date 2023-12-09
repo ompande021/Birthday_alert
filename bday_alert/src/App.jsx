432334d1bfcf1b5fa2f97e47341531c0d66c99dc
ompande021 <97015947+ompande021@users.noreply.github.com>
2023-12-09 13:28:14 +0530

    add files

---
+++ bday_alert/src/App.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react"
+import Data from "./ApiData"
+
+
+const DataIndex = () =>{
+
+    const[data,setData]=useState(Data);
+    let d=new Date();
+    var date=d.getDate()+'/'+d.getMonth()+'/'+d.getFullYear();
+    return(
+        <div id="box">
+            <div id="box1">
+                <h2>Today's BDay🎂🎉</h2>
+                <u>({date})</u>
+                <div>
+                    {data.map((e)=>{
+                        return(
+                            <div key={e.name} id="box2">
+                                <img src={e.image} alt="" />
+                                <div>
+                                <p>
+                                    <h3>{e.name}</h3>
+                                    ({e.age})
+                                </p>
+                                
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+                <button onClick={()=>setData([]) }>Clear All</button>
+                
+            </div>
+        </div>
+    )
+}
+export default DataIndex
+
+
+
+/*import Data from "./CreateAPI/DataIndex"
+
+const App = () =>{
+   
+    return(
+        <div>
+            <Data/>
+        </div>
+    )
+}
+export default App */
